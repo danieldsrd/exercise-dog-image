01660a2082db0a16af4f3c26d6c8b1bdd37423ab
Daniel <danieldsrd@gmail.com>
2021-01-20 18:40:52 -0300

    Exercicio 1

--- src/App.js
+++ src/App.js
@@ -2,25 +2,56 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      dogObj: undefined,
+      loading: true,      
+    };
+    this.renderDogImage = this.renderDogImage.bind(this);
+    this.newDog = this.newDog.bind(this);
+  }
+
+  async fetchDog() {
+    const requestHeaders = { headers: { Accept: 'application/json' } };
+    const requestReturn = await fetch('https://dog.ceo/api/breeds/image/random', requestHeaders);
+    const requestObject = await requestReturn.json();    
+    this.setState({
+      dogObj: requestObject.message,
+    });
+  }
+
+  newDog() {
+    this.fetchDog();
+    console.log('count click');
+  }
+
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  renderDogImage() {
+    return (
+      <div>
+        <img src={this.state.dogObj} id="img-change" alt="dog"/>
+        <button type="button" onClick={this.newDog}>
+          Novo Doguinho!
+        </button>
+      </div>
+    )
+  }
+
+  render() {
+    const { dogObj } = this.state;
+    const loadingElement = <span>Loading...</span>
+    return (
+      <div>
+        <p>{ dogObj ? this.renderDogImage() : loadingElement }</p>
+      </div>
+    )
+  }
 }
 
 export default App;
